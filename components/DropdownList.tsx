71985ffec09e52aab37e613ac110e7638e271891
AbdullahSajid18 <abdullahsajid1098@gmail.com>
2025-05-14 22:48:33 +0500

    implement dropdown list

--- components/DropdownList.tsx
+++ components/DropdownList.tsx
@@ -10,14 +10,31 @@ const DropdownList = () => {
     <div className="relative">
       <div className="cursor-pointer" onClick={() => seIsOpen(!isOpen)}>
         <div className="filter-trigger">
-          <Image 
-            src='/assets/icons/hamburger.svg'
-            alt="menu"
-            width={14}
-            height={14}
+          <figure>
+            <Image
+              src="/assets/icons/hamburger.svg"
+              alt="menu"
+              width={14}
+              height={14}
+            />
+            Most recent
+          </figure>
+          <Image
+            src="/assets/icons/arrow-down.svg"
+            alt="arrow-down"
+            width={20}
+            height={20}
           />
         </div>
       </div>
+
+      {isOpen && (
+        <ul className="dropdown">
+          {["Most recent", "Most liked"].map((option) => (
+            <li key={option} className="list-item">{option}</li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
